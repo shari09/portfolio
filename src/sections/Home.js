c1b3890911b3c557581e092511d23a09749cdc20
Shari Sun <shari09cxxcr@gmail.com>
2019-12-09 12:32:19 -0500

    push for now

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -6,7 +6,7 @@ import {screenSizes, colours} from '../constants.js';
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 20px;
   width: 100%;
   height: 100vh;
